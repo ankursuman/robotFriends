994061e5e85a18bf90846a3a80bf50e94d0b8087
Ankur Kumar Suman <50414074+ankursuman@users.noreply.github.com>
2020-09-08 19:00:05 +0530

    Updates

---
+++ precache-manifest.feebda62357ce24700debc1238df3b07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0b4ad06a6f6af66c10a55e8d5e4bac",
+    "url": "/robotFriends/index.html"
+  },
+  {
+    "revision": "b5118dddd82f44ec37f9",
+    "url": "/robotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "08b3b723b8ecd7b178e7",
+    "url": "/robotFriends/static/css/main.70ff799e.chunk.css"
+  },
+  {
+    "revision": "b5118dddd82f44ec37f9",
+    "url": "/robotFriends/static/js/2.a1f43411.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robotFriends/static/js/2.a1f43411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b3b723b8ecd7b178e7",
+    "url": "/robotFriends/static/js/main.0f21250b.chunk.js"
+  },
+  {
+    "revision": "70f4d5dc6da80a554205",
+    "url": "/robotFriends/static/js/runtime-main.e0196c21.js"
+  }
+]);
